5707f144d09cf1222701d12b95f874225ecc0d52
mstencel <michal.stencel28@gmail.com>
2023-03-04 17:28:30 +0100

    2 nodes

--- lab1/test.sh
+++ lab1/test.sh
@@ -1,5 +1,5 @@
 for i in {1..1000}
 do
     a=$(( 100*i ))
-    mpiexec -machinefile ./2node -np 1 ./ping_pong.py $a >> results_2.csv
+    mpiexec -machinefile ./2node -np 2 ./ping_pong.py $a >> results_2.csv
 done
