30dd4241f934570750c5361b5e07cecb880e013d
mstencel <michal.stencel28@gmail.com>
2023-03-04 16:09:52 +0100

    ping pong change

--- lab1/ping_pong.py
+++ lab1/ping_pong.py
@@ -6,14 +6,16 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 MESSAGES_AMOUNT = 10000
+bytes_amount = int(sys.argv[1])
+message = "X" * bytes_amount
 
 
-def ping_pong(ping_pong_message):
+def ping_pong(_):
     comm.Barrier()
     start = MPI.Wtime()
 
     if rank == 0:
-        comm.send(ping_pong_message, dest=1)
+        comm.send(message, dest=1)
         comm.recv(source=1)
     elif rank == 1:
         data = comm.recv(source=0)
@@ -24,9 +26,7 @@ def ping_pong(ping_pong_message):
     return stop - start
 
 
-bytes_amount = int(sys.argv[1])
-message = "X" * bytes_amount
-total_time = reduce(lambda t1, t2: t1 + t2, map(ping_pong(message), range(MESSAGES_AMOUNT)))
+total_time = sum(map(ping_pong, range(MESSAGES_AMOUNT)))
 
 if rank == 0:
     result = total_time / MESSAGES_AMOUNT
