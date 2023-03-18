994a6213aacfead3f456867301981f57bda658f5
mstencel <michal.stencel28@gmail.com>
2023-03-18 14:57:42 +0100

    lab2 change

--- lab2/run.sh
+++ lab2/run.sh
@@ -1,32 +1,19 @@
-#!/bin/bash
-
-# Job configuration
-
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
-#SBATCH --time=01:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr23-cpu
 
-# Variables
-#number_of_points="5000000 41833001 350000000"
-number_of_points="5000 41833 350000"
-# Load modules
 module add scipy-bundle/2021.10-foss-2021b
 
-# pi calculation
-run_pi_calculation() {
-    local num_points=$1
-    local num_processors=$2
-    mpiexec -np "$num_processors" ./pi.py "$num_points"
-}
-
-# Loop through number of points and run pi calculation
-for num_points in $number_of_points; do
-    for ((i=1; i<=15; i++)); do
-        for ((num_processors=1; num_processors<=12; num_processors++)); do
-            run_pi_calculation "$num_points" "$num_processors"
+for totalPoints in 4000000 34641016 300000000
+do
+    for threads in {1..12}
+    do
+        for i in {1..10}
+        do
+            mpiexec -np $threads ./pi.py $totalPoints
         done
     done
 done
