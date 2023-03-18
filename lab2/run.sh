5694a2c50e47c70ee5a34ff430a6a83a18171f0e
mstencel <michal.stencel28@gmail.com>
2023-03-18 14:29:52 +0100

    pi script calculation

---
+++ lab2/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Job configuration
+#SBATCH --job-name=pi-calculation
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=12:00:00
+#SBATCH --partition=plgrid
+#SBATCH --account=plgmpr22
+
+# Variables
+#number_of_points="5000000 41833001 350000000"
+number_of_points="5000 41833 350000"
+
+# Load modules
+module load plgrid/tools/openmpi
+module load plgrid/libs/python-mpi4py/3.0.0-python-2.7
+
+# pi calculation
+run_pi_calculation() {
+    local num_points=$1
+    local num_processors=$2
+    mpiexec -np "$num_processors" ./pi.py "$num_points"
+}
+
+# Loop through number of points and run pi calculation
+for num_points in $number_of_points; do
+    for ((i=1; i<=15; i++)); do
+        for ((num_processors=1; num_processors<=12; num_processors++)); do
+            run_pi_calculation "$num_points" "$num_processors"
+        done
+    done
+done
