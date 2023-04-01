6ea68fbae631393e2f57a282899047583293d9e0
stencel <stencel@dc1a-lab-vnode-01.ipa.ki.agh.edu.pl>
2023-04-01 19:57:39 +0200

    lab4

--- lab4/run.sh
+++ lab4/run.sh
@@ -1,6 +1,6 @@
 filename="$1.csv"
 
-rm filename
+rm $filename
 
 values=10000000,50000000,100000000
 
@@ -8,6 +8,6 @@ for val in ${values//,/ }
 do
     for threads in {1..10}
     do
-        ./loop $threads $val >> filename
+        ./loop $threads $val >> $filename
     done
 done
