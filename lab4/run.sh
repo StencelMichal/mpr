cc94b61fe2a15eb1c279565868c8147b5afd8795
mstencel <michal.stencel28@gmail.com>
2023-04-01 19:42:36 +0200

    lab4

---
+++ lab4/run.sh
@@ -0,0 +1,13 @@
+filename="$1.csv"
+
+rm filename
+
+values=10000000,50000000,100000000
+
+for val in ${values//,/ }
+do
+    for threads in {1..5}
+    do
+        ./loop $threads $val >> filename
+    done
+done
