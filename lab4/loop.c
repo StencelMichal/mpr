03b7d97dcd06aa0af5f7e7ef36ef727c71b3806e
mstencel <michal.stencel28@gmail.com>
2023-04-02 14:55:59 +0200

    lab4

--- lab4/loop.c
+++ lab4/loop.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
     start = omp_get_wtime();
     #pragma omp parallel private(seed)
     {
-        #pragma omp for schedule(auto)
+        #pragma omp for schedule(runtime)
             for(i = 0; i < array_size; i++){
                 numbers[i] = erand48(seed);
             }
