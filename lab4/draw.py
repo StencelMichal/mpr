534eb350652aca3bb9264844c1a4c375ea5df623
mstencel <michal.stencel28@gmail.com>
2023-04-02 15:31:32 +0200

    lab4

--- lab4/draw.py
+++ lab4/draw.py
@@ -1,49 +1,42 @@
-filename = 'default.csv'
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Wczytaj plik csv z separatorem średnika
-data = pd.read_csv(filename, header=None, sep=';')
+filenames = ['static.csv', 'dynamic.csv', 'guided.csv', 'runtime.csv', 'auto.csv']
 
-# Grupuj dane po rozmiarze tablicy (kolumna 0)
-groups = data.groupby(data.iloc[:, 0])
+for filename in filenames:
+    data = pd.read_csv(filename, header=None, sep=';')
 
-# Twórz wykresy z czasem wykonania dla każdej grupy
-fig_czasu, ax1 = plt.subplots()
-fig_czasu.set_size_inches(8, 6)
+    groups = data.groupby(data.iloc[:, 0])
 
-for group_name, group_data in groups:
-    ax1.plot(group_data.iloc[:, 1], group_data.iloc[:, 2], 'o-', label=f'rozmiar_tablicy: {group_name}')
+    fig_czasu, ax1 = plt.subplots()
+    fig_czasu.set_size_inches(8, 6)
 
-ax1.set_xlabel('liczba wątków')
-ax1.set_ylabel('Czas wykonania [s]')
-ax1.set_title('Czas wykonania dla różnych rozmiarów tablicy')
-ax1.legend()
-ax1.grid(True)
+    for group_name, group_data in groups:
+        ax1.plot(group_data.iloc[:, 1], group_data.iloc[:, 2], 'o-', label=f'rozmiar_tablicy: {group_name}')
 
-# Ustawienie osi x - zaznaczenie wszystkich ilości wątków
-ax1.set_xticks(data.iloc[:, 1].unique())
+    ax1.set_xlabel('liczba wątków')
+    ax1.set_ylabel('Czas wykonania [s]')
+    ax1.set_title(f'Czas wykonania dla różnych rozmiarów tablicy - {filename[:-4]}')
+    ax1.legend()
+    ax1.grid(True)
 
-# Zapisz wykres czasu do pliku PNG
-fig_czasu.savefig('wykres_czasu.png', dpi=300)
+    ax1.set_xticks(data.iloc[:, 1].unique())
 
-# Twórz wykresy z przyśpieszeniem dla każdej grupy
-fig_przyspieszenia, ax2 = plt.subplots()
-fig_przyspieszenia.set_size_inches(8, 6)
+    fig_czasu.savefig(f'wykres_czasu_{filename[:-4]}.png', dpi=300)
 
-for group_name, group_data in groups:
-    speedup = group_data.iloc[0, 2] / group_data.iloc[:, 2]
-    ax2.plot(group_data.iloc[:, 1], speedup, 'o-', label=f'rozmiar_tablicy: {group_name}')
+    fig_przyspieszenia, ax2 = plt.subplots()
+    fig_przyspieszenia.set_size_inches(8, 6)
 
-ax2.set_xlabel('liczba wątków')
-ax2.set_ylabel('Przyśpieszenie')
-ax2.set_title('Przyśpieszenie dla różnych rozmiarów tablicy')
-ax2.legend()
-ax2.grid(True)
+    for group_name, group_data in groups:
+        speedup = group_data.iloc[0, 2] / group_data.iloc[:, 2]
+        ax2.plot(group_data.iloc[:, 1], speedup, 'o-', label=f'rozmiar_tablicy: {group_name}')
 
-# Ustawienie osi x - zaznaczenie wszystkich ilości wątków
-ax2.set_xticks(data.iloc[:, 1].unique())
+    ax2.set_xlabel('liczba wątków')
+    ax2.set_ylabel('Przyśpieszenie')
+    ax2.set_title(f'Przyśpieszenie dla różnych rozmiarów tablicy - {filename[:-4]}')
+    ax2.legend()
+    ax2.grid(True)
 
-# Zapisz wykres przyśpieszenia do pliku PNG
-fig_przyspieszenia.savefig('wykres_przyspieszenia.png', dpi=300)
+    ax2.set_xticks(data.iloc[:, 1].unique())
+
+    fig_przyspieszenia.savefig(f'wykres_przyspieszenia_{filename[:-4]}.png', dpi=300)
